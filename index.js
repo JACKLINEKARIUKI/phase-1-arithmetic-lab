363b788570aee889766f75847d7bdfacb6e9bfcc
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-17 16:24:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(12, 19, 20, 10, 5);
